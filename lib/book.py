8df780428378a3ad7042772bdc42b999607df183
Historia Montague <historia.montague@outlook.com>
2023-05-23 11:32:08 -0600

    Finished Lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+    
+    def set_title(self):
+        return self._title
+    
+    def set_page_count(self):
+        return self._page_count
+    
+    def get_title(self, title):
+        self._title = title
+
+    def get_page_count(self, page_count):
+        try:
+            if(type(page_count) == int):
+                self._page_count = page_count  
+            else:
+                raise TypeError("")            
+        except TypeError:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+    title = property(set_title, get_title)
+    page_count = property(set_page_count, get_page_count)
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+    
+    def set_size(self):
+        return self._size
+    
+    def set_brand(self):
+        return self._brand
+    
+    def get_size(self, size):
+        try:
+            if(type(size) == int):
+                self._size = size
+            else:
+                raise TypeError
+        except TypeError:
+            print("size must be an integer")
+ 
+    def get_brand(self, brand):
+        self._brand = brand
+    
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+        
+    brand = property(set_brand, get_brand)
+    size = property(set_size, get_size)
